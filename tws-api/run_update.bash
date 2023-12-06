5830d9ee54239323163cbb651e294b0ce4c55518
bakr_ouairem <bakr.ouairem@icloud.com>
2023-12-06 14:07:37 +0100

    Quick correction of a bug on the 10.25 version of ibapi : Adding parent module

--- tws-api/run_update.bash
+++ tws-api/run_update.bash
@@ -2,6 +2,10 @@
 
 # Version Python 
 python -V
+pip -V 
+
+# Forcing the same pip dist
+alias pip="python -m pip"
 
 # Get the absolute path to the directory containing this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -21,8 +25,19 @@ ABSOLUTE_FOLDER_PATH="$( cd "$SCRIPT_DIR/$RELATIVE_FOLDER_PATH" && pwd )"
 cd $ABSOLUTE_FOLDER_PATH
 pwd
 
-# Run Python script with absolute path
-echo "$(python "setup.py" install)" >> "$SCRIPT_DIR/$LOG_FOLDER/build_logs.txt"
+# Run Python script with absolute path : first we create a dist
+echo "STEP 1: Creating distribution " >> "$SCRIPT_DIR/$LOG_FOLDER/build_logs.txt"
+echo "$(python "setup.py" sdist)" >> "$SCRIPT_DIR/$LOG_FOLDER/build_logs.txt"
+
+# Dist idea
+cd dist 
+DISTRIBUTION="$(ls -t | head -n1)"
+echo "Distribution made : $DISTRIBUTION" >> "$SCRIPT_DIR/$LOG_FOLDER/build_logs.txt"
+cd ../
+
+# Running Python script to install the path
+echo "STEP 2: Installing dist " >> "$SCRIPT_DIR/$LOG_FOLDER/build_logs.txt"
+echo "$(pip install dist/$DISTRIBUTION)" >> "$SCRIPT_DIR/$LOG_FOLDER/build_logs.txt"
 
 # Running an install test
 pip show ibapi
